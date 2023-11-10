36c5ade40f4ae78d5559f14e7d178ffeb2ba4060
Damaysah <112998606+Damaysah@users.noreply.github.com>
2023-11-10 15:55:44 +0700

    jawaban soal 2

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('/mahasiswa')->group(function () {
-    Route::get('/pendaftaran', function () {
-        return 'Halaman pendaftaran untuk pendaftaran';
-    });
+// routes/web.php
 
-    Route::get('/ujian', function () {
-        return 'Halaman Ujian untuk ujian';
-    });
-
-    Route::get('/nilai', function () {
-        return 'Halaman nilai untuk nilai';
-    });
+Route::get('/', function () {
+    return view('mahasiswa');
 });
 
